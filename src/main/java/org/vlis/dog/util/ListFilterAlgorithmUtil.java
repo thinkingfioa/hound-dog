ff16c1d10b01bf19b8d357d53e43bd6b137f98ea
thinking_fioa <thinking_fioa@163.com>
2017-11-22 11:51:01 +0800

    algorithm

---
+++ src/main/java/org/vlis/dog/util/ListFilterAlgorithmUtil.java
@@ -0,0 +1,11 @@
+package org.vlis.dog.util;
+
+/**
+ * @author thinking_fioa
+ * @createTime 2017/11/22
+ * @description 去重复算法
+ */
+
+
+public final class ListFilterAlgorithmUtil {
+}
---
+++ src/main/java/org/vlis/dog/util/ReasoningAlgorithmUtil.java
@@ -0,0 +1,11 @@
+package org.vlis.dog.util;
+
+/**
+ * @author thinking_fioa
+ * @createTime 2017/11/22
+ * @description 提供推理算法
+ */
+
+
+public final class ReasoningAlgorithmUtil {
+}
